df1c12db34336e87ec17bb0a7f57ec847b33df74
GordoDeGuerra <j.cruz19@escola.pr.gov.br>
2026-07-27 00:36:59 -0300

    Add event listener for button click

--- style.js
+++ style.js
@@ -1,1 +1,6 @@
-
+function eventobotao() {
+console.log("botão clicado");
+alert("formulário enviado, valeu.");
+}
+const botaoformulario = document.getElementById("botao1");
+botaoformulario.addEventListener('click', eventobotao);
